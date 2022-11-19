60e45dcc114dd26a829f85a3cb826f452a2e5701
Ajaxlal <ajaxlal1@gmail.com>
2022-11-19 11:43:19 +0530

    Project setup

---
+++ DemoApp/src/constants/assets.js
@@ -0,0 +1,1 @@
+export {default as LOGO} from '../assets/images/SplashLogo/splashlogo.png';
---
+++ DemoApp/src/containers/SplashScreen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+import styles from "./style";
+import {LOGO} from '../../constants/assets'
+const SplashScreen = () =>{
+
+    return (
+        <View  style = {styles.mainContainer}>
+<Image source={}/>
+        </View>
+    )
+}
